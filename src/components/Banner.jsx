8152fcf9f6ac970c2015a4acd2e829015d85af26
Daniyal Syed <daniyal.syed@live.com>
2023-11-15 13:00:49 -0600

    task 2 banner component

---
+++ src/components/Banner.jsx
@@ -0,0 +1,10 @@
+export default function Banner() {
+    return (
+        <div>
+            <header>Orbit Report</header>                                    
+            <div>
+                <p>Click on the buttons to see the satellites in that orbit type</p>
+            </div>
+        </div>
+    )      
+} 
